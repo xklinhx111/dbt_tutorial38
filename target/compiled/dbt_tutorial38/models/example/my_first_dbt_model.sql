0fe7dd658a8bb855f5fc584f9d0ba24ae96f9c45
Kim Linh <MY_NAME@example.com>
2025-12-02 20:02:06 +0100

    Add dbt sources for raw tables

--- target/compiled/dbt_tutorial38/models/example/my_first_dbt_model.sql
+++ target/compiled/dbt_tutorial38/models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -7,7 +6,7 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+
 
 with source_data as (
 
